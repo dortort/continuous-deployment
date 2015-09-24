1106d3a68b3b623f5dcf9233769341f0111eec60
Francis Dortort <francis@dortort.com>
2015-09-24 13:39:50 +0200

    DOC_ROOT env variable

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # git clone
-git clone ${GIT_REPO} /var/www/html \
+git clone $GIT_REPO /var/www/html \
         && chown -R www-data:www-data /var/www/html
+
+# document root
+sed -e "s|^DocumentRoot .*|DocumentRoot \"/var/www/html$DOC_ROOT\"|g" \
+        -i /etc/apache2/apache2.conf
 
 # start apache
 exec apache2-foreground
