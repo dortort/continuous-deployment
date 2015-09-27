aeb980910876363d2c460ef37ca1954841c9d555
Francis Dortort <francis@dortort.com>
2015-09-27 02:28:28 +0200

    new data dir for git clone

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -4,13 +4,13 @@
 cd /
 
 # git clone
-rm -rf /var/www/html \
-        && mkdir -p /var/www/html \
-        && git clone $GIT_REPO --branch ${GIT_BRANCH:-"master"} /var/www/html \
-        && chown -R www-data:www-data /var/www/html
+rm -rf /data/app \
+        && mkdir -p /data/app \
+        && git clone $GIT_REPO --branch ${GIT_BRANCH:-"master"} /data/app \
+        && chown -R www-data:www-data /data/app
 
 # document root
-sed -e "s|^DocumentRoot .*|DocumentRoot \"/var/www/html$DOC_ROOT\"|g" \
+sed -e "s|^DocumentRoot .*|DocumentRoot \"/data/app$DOC_ROOT\"|g" \
         -i /etc/apache2/apache2.conf
 
 # start apache
